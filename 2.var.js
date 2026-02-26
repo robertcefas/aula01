9c293b86e5ad229f62fd4ae47d437bd8ad419949
BrunoHsdev <bhas795002@gmail.com>
2026-02-26 20:33:36 -0300

    opçoes

---
+++ 2.var.js
@@ -0,0 +1,6 @@
+//Variáveis.
+
+//Forma maus antiga
+var nome = "João";
+
+console.log("Nome: ", nome);
---
+++ 6.sswitch_case.js
@@ -0,0 +1,25 @@
+//Switch - Case
+
+const opcao = 3;
+
+switch (opcao) {
+    case 1:
+        console.log("Opção 1 ")
+        break;
+    case 2:
+        console.log("Opção 2 ")
+        break;
+    case 3:
+        console.log("Opção 3 ") //alt shift e seta para baixo para copiar a linha
+        break;
+        console.log("Opção inválida") //control de pra selecionar a opção
+    case 4:
+        console.log("Opção 4 ")
+        break;
+    case 5:
+        console.log("Opção 5 ")
+        break;
+    default:
+        console.log("Opção inválida")//alt e sertar para selecionar a opção
+        break;
+}
