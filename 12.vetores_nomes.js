f573cd045635de1a9738c32c3a123c553fdeedbf
carlossantos01 <carlossantosgithub@gmail.com>
2026-02-27 20:56:35 -0300

    doc 13

---
+++ 12.vetores_nomes.js
@@ -0,0 +1,31 @@
+//Criando um vetor
+
+const listaNomes =["Marta","julia","Ana"]
+console.log("\nExibindo todos os nomes")
+console.log(listaNomes)
+
+
+console.log("\nExibindo o primeiro elemento: ")
+console. log(listaNomes [0])
+
+console.log("\nExibindo o segundo elemento: ")
+console.log(listaNomes [1])
+
+console.log("\nExibindo o segundo elemento: ")
+console.log(listaNomes [2])
+
+console.log("\nAdicionando um elemento: ")
+listaNomes.push ("Mariana")
+console.log (listaNomes)
+
+console.log("\nRemovendo o primeiro elemento: ")
+listaNomes. shift()
+console.log(listaNomes)
+
+console.log("\nRemovendo o último elemento:")
+listaNomes.pop()
+console.log(listaNomes)
+console.log ("\nRemovendo um elemento específico: ")
+listaNomes.split(1,1)
+console.log(listaNomes)
+ 
