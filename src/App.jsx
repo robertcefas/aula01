974fa475c8b87cfe76a00f05f2a268038a46dd13
Aluno Dendezeiros <aluno.den@senaibahia.com.br>
2026-03-26 21:35:26 -0300

    Na aula nova

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import { Mensagem } from './components/Mensagem'
+
+function App() {
+
+  return (
+    <>
+    <h1>Bem vindo ao meu projeto!!</h1>
+
+    <Mensagem />
+    <Mensagem />
+    <Mensagem />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Mensagem/index.jsx
@@ -0,0 +1,11 @@
+import './style.css'
+
+export function Mensagem() {
+    return (
+        <div className='mensagem'>
+            <h1>Componentes</h1>
+            <p>Este é um componete simples.</p>
+        </div>
+        )
+}
+// export default Mensagem
